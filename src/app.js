032116b1805727b446a220f5b9fe6fa94f28fca2
longvv <vvlong90@gmail.com>
2024-12-05 09:52:22 +0700

    commit

---
+++ src/app.js
@@ -0,0 +1,70 @@
+const helmets = require('helmet');
+const express = require('express');
+const cors = require('cors');
+require('dotenv').config();
+
+const app = express();
+
+// Middleware
+app.use(helmets());
+app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+//Basic API
+app.get('/',(req, res) => {
+    res.json({
+        message: 'Hello, World!'
+    })
+});
+
+app.get('/users/:id', async (req, res, next) => {
+    try {
+        res.json({
+            message: 'User fetched successfully',
+            user: {
+                id: req.params.id,
+                name: 'John Doe'
+            }
+        });
+    }
+    catch (error) {
+        next(error);
+    }
+});
+
+//Get server heath
+app.get('/health', (req, res, next) => {
+    try {
+        var _uptime = Math.round(process.uptime());
+        res.json({
+            status: 'OK',
+            time: new Date(),
+            uptime: _uptime,
+        });
+    } catch (error) {
+        next(error);
+    }
+});
+
+// Error handling middleware
+app.use((err, req, res, next) => {
+    console.error(err.stack);
+    res.status(500).json({ 
+        status: 'Internal Server Error',
+        error: err.message,
+    });
+});
+
+app.use((err, req, res, next) => {
+    res.status(400).json({
+        status: 'Bad Request',
+        error: err.message,
+    });
+});
+
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT} in ${process.env.NODE_ENV} mode`);
+});
